e38d8ee3715c4a82a4c9709b25478be79391f670
shubham tiwari <shubhamsriit1995@gmail.com>
2025-01-28 14:09:11 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -78,6 +78,18 @@ function NavBar() {
                 Projects
               </Nav.Link>
             </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/resume"
+                onClick={() => updateExpanded(false)}
+              >
+                <AiOutlineFundProjectionScreen
+                  style={{ marginBottom: "2px" }}
+                />{" "}
+                Resume
+              </Nav.Link>
+            </Nav.Item>
 
             <Nav.Item className="fork-btn">
               <Button
